5f2b9fd4a48eb11a7943fc7e9fe07815b5a066fc
TomoharuUchida <tomou0711@gmail.com>
2022-04-18 15:21:01 +0900

    get events,render events list

--- react-redux-crud-application/src/index.js
+++ react-redux-crud-application/src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createStore } from "redux";
+import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom/client';
+import { createStore,applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import './index.css';
 import reducer from "./reducers"
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(thunk));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
-);
+    <EventsIndex />
+  </Provider>,
+  document.getElementById("root")
+)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <EventsIndex />
+//     </React.StrictMode>
+//   </Provider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
