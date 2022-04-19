78dbb9bd1e8b9e1c65317fbb4650af9c32835b35
TomoharuUchida <tomou0711@gmail.com>
2022-04-19 12:30:13 +0900

    icrement feature UPDATE

--- react-redux-crud-application/src/reducers/events.js
+++ react-redux-crud-application/src/reducers/events.js
@@ -1,12 +1,21 @@
 import _ from "lodash";
 import {
+    CREATE_EVENT,
     READ_EVENTS,
+    READ_EVENT,
+    UPDATE_EVENT,
     DELETE_EVENT,
 } from "../actions"
 
 
 export default (events={}, action) => {
     switch (action.type) {
+        case CREATE_EVENT:
+        case READ_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data;
+            // 先にconsole.log(action.response.data)でデータの構造を確認するとよい
+            return { ...events, [data.id]: data }
         case READ_EVENTS:
             // 配列ではなくオブジェクトの形にする
             return _.mapKeys(action.response.data, "id")
