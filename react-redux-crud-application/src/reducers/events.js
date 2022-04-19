340fc8bdc451b78aa4318053eda7a5c3ad2ff5dd
TomoharuUchida <tomou0711@gmail.com>
2022-04-19 09:44:58 +0900

    increment delete feature

--- react-redux-crud-application/src/reducers/events.js
+++ react-redux-crud-application/src/reducers/events.js
@@ -1,11 +1,19 @@
-import { READ_EVENTS } from "../actions"
 import _ from "lodash";
+import {
+    READ_EVENTS,
+    DELETE_EVENT,
+} from "../actions"
+
 
 export default (events={}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             // 配列ではなくオブジェクトの形にする
             return _.mapKeys(action.response.data, "id")
+        case DELETE_EVENT:
+            delete events[action.id];
+            // スプレッド構文で返す
+            return{...events}
         default:
             return events
     }
